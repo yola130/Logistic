8a2d506065e7749b36635cc0ffeb36fe7f48c8cb
yola130 <1305451838@qq.com>
2024-12-21 12:10:55 +0800

    Update Logistic.py

--- Logistic.py
+++ Logistic.py
@@ -3,39 +3,38 @@ import joblib
 import numpy as np
 
 
-def main():
     # Load the model
-    model = joblib.load('Logistic.pkl')
+model = joblib.load('Logistic.pkl')
  
     # Define feature names
-    feature_names = ["LDH", "ALP", "Neutrophils", "DBIL", "ALB", "Fg"]
+feature_names = ["LDH", "ALP", "Neutrophils", "DBIL", "ALB", "Fg"]
 
     # Streamlit user interface
-    st.title("COVID-19 Subphenotype Classifier")
+st.title("COVID-19 Subphenotype Classifier")
  
     # LDH: numerical input
-    LDH = st.number_input("LDH:", min_value=50, max_value=4000, value=270)
+LDH = st.number_input("LDH:", min_value=50, max_value=4000, value=270)
 
     # ALP: numerical input
-    ALP = st.number_input("ALP:", min_value=1, max_value=2000, value=80)
+ALP = st.number_input("ALP:", min_value=1, max_value=2000, value=80)
 
     # Neutrophils: numerical input
-    Neutrophils = st.number_input("Neutrophils:", min_value=0, max_value=50, value=6)
+Neutrophils = st.number_input("Neutrophils:", min_value=0, max_value=50, value=6)
 
     # DBIL: numerical input
-    DBIL = st.number_input("DBIL:", min_value=0, max_value=100, value=5)
+DBIL = st.number_input("DBIL:", min_value=0, max_value=100, value=5)
 
     # ALB: numerical input
-    ALB = st.number_input("ALB:", min_value=0, max_value=100, value=35)
+ALB = st.number_input("ALB:", min_value=0, max_value=100, value=35)
 
     # Fg: numerical input
-    Fg = st.number_input("Fg:", min_value=0, max_value=50, value=3)
+Fg = st.number_input("Fg:", min_value=0, max_value=50, value=3)
 
     # Process inputs and make predictions
-    feature_values = [LDH, ALP, Neutrophils, DBIL, ALB, Fg]
-    features = np.array([feature_values])
+feature_values = [LDH, ALP, Neutrophils, DBIL, ALB, Fg]
+features = np.array([feature_values])
 
-    if st.button("Predict"):
+if st.button("Predict"):
         # Predict probabilities
         predicted_proba = model.predict_proba(features)[0]
 
@@ -57,5 +56,3 @@ def main():
         st.subheader(advice, anchor=False)
 
 
-if __name__ == "__main__":
-    main()
